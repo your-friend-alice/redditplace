7d81bb48e8ceb07ced27f34cdffbed97f17191cd
Devon Sawatzky <d@dn3s.me>
2017-03-31 23:59:39 -0500

    fix views that go out of bounds
    Now terminal cells outside the area are rendered as space characters
    with no fill, to improve compatibility with transparent terminals. Also,
    it no longer crashes when viewing out-of-bounds areas, so some (only
    partially effective) limits are removed

--- redditplace.py
+++ redditplace.py
@@ -15,7 +15,7 @@ def valueAt(x, y):
     Get the color value at a given coordinate on the Place canvas
     """
     if(x<0 or x>999 or y<0 or y>999):
-        return 3
+        return None
     byte=data[y*500+int((x)/2)+4]
     if x%2 == 0:
         return byte >> 4
@@ -30,6 +30,16 @@ def colorChar(top, bott, optimize=True):
     True, don't print color codes if last time the color was the same.
     """
     global prevColor
+    if(top==None or bott==None):
+        out='\033[0m'
+        if top==bott==None:
+            out+=" " if prevColor[0]!=None or prevColor[1]!=None else " "
+            prevColor=(top,bott)
+            return out
+        out+='\033[38;5;'+colors[top or bott]+'m'
+        out+='▄' if bott else '▀'
+        prevColor=(top,bott)
+        return out
     topCode='\033[38;5;' +colors[top] +'m' if top  != prevColor[0] else ""
     bottCode='\033[48;5;'+colors[bott]+'m' if bott != prevColor[1] else ""
     prevColor=(top,bott)
@@ -42,8 +52,8 @@ def printAt(x, y, w=None, h=None):
     """
     w=w or os.get_terminal_size()[0]
     h=h or os.get_terminal_size()[1]-1
-    startX=max(0, int(x-w/2))
-    startY=max(0, int(y-h))
+    startX=int(x-w/2)
+    startY=int(y-h)
     rows=[]
     for y in range(startY,startY+h*2,2):
         rows.append("".join(colorChar(valueAt(x,y), valueAt(x,y+1)) for x in range(startX,startX+w)))
